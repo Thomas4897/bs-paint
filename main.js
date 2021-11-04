fc219571c7b0f60995a71aa41232d0723b6702fa
Thomas4897 <t.maynard4897@gmail.com>
2021-11-05 04:14:17 +0800

    Finishing Basic Code

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 /*******************
  * OUR HELPER CODE *
-*******************/
+ *******************/
 
 /*
  * Here we add the squares to the canvas dynamically.
  * You can mostly leave this section alone!
  * But if you want to change how wide the canvas is,
  * there are just two steps:
- * 
+ *
  * 1. Change the `gridWidth` value below.
  * 2. Change the `grid-template-rows` and
  * `grid-template-columns` to match.
@@ -15,59 +15,96 @@
  * To make the second one happen, the number to change
  * is the first argument to `repeat`, currently set at 10.
  */
-const gridWidth = 10;
+const gridWidth = 50;
 let count = 0;
-while (count <= gridWidth * gridWidth) {
-  const canvas = document.querySelector('.canvas');
-  const div = document.createElement('div');
-  div.className = 'square color-5';
-  canvas.appendChild(div);
-  count++;
+while (count < gridWidth * gridWidth) {
+	const canvas = document.querySelector(".canvas");
+	const div = document.createElement("div");
+	div.className = "square color-5";
+	canvas.appendChild(div);
+	count++;
 }
 
-// You probably should NOT do these in the order below.
-// That is, you probably should NOT do all the queries,
-// THEN all the functions,
-// THEN all the wiring.
+const app = document.querySelector(".app");
 
-// Instead, it'll be easier if you go one action at a time!
-// So, add a query for the palette colors.
-// THEN add an event listener function for what happens when one is clicked.
-// THEN wire those two together, so that when the palette elements are clicked,
-// the function runs.
-//
-// And proceed from there to getting the squares working.
-//
+const colorSelector = document.querySelector(".palette");
 
-// ALSO.
-// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
+const currentBrush = document.querySelector(".current-brush");
 
-/***********
- * QUERIES *
-***********/
+const newBrush = colorSelector.addEventListener("click", function (event) {
+	const currentColor = currentBrush.classList[1];
+	// console.log("event.target", event.target.classList[1]);
+	if (event.target.localName === "div") {
+		currentBrush.classList.replace(currentColor, event.target.classList[1]);
+	}
+});
 
-// Add queries for all your squares, palette colors, and brush here.
-// (Note the singular or plural used in that sentence!)
+const canvas = document.querySelector(".canvas");
 
+const paint = canvas.addEventListener("click", function (event) {
+	const currentColor = currentBrush.classList[1];
+	const square = event.target.classList[1];
+	console.log("event.target", square);
+	if (event.target.localName === "div") {
+		event.target.classList.replace(square, currentColor);
+	}
+});
 
+let clicked = false;
 
-/****************************
- * EVENT LISTENER FUNCTIONS *
-****************************/
+const mouseDown = document.addEventListener("mousedown", function () {
+	clicked = true;
+});
 
-// Now add some functions to handle clicking one particular square
-// and clicking one particular palette color. You can leave them
-// empty at first, though a console.log just to know they're being
-// run as event listeners (after the next step is set up) isn't a
-// bad idea for testing purposes.
+const mouseUp = document.addEventListener("mouseup", function () {
+	clicked = false;
+});
 
+const paintDrag = canvas.addEventListener("mousemove", function (event) {
+	const currentColor = currentBrush.classList[1];
+	const square = event.target.classList[1];
+	console.log("event.target", square);
+	if (clicked === true) {
+		if (event.target.localName === "div") {
+			event.target.classList.replace(square, currentColor);
+		}
+	}
+});
 
+const body = document.querySelector("body");
 
-/**************************
- * WIRING IT ALL TOGETHER *
-**************************/
+const darkModeButton = document.querySelector(".dark-mode-button");
 
-// Now: wiring up our event listeners to our html node elements.
-// You'll need to add the appropriate event listener for each
-// square and for each palette color from the functions you
-// wrote above.
+const headings = document.querySelector(".headings");
+
+const icon = document.querySelector(".icon");
+
+darkModeButton.style.cursor = "pointer";
+const changeTheme = darkModeButton.addEventListener("click", function () {
+	// if (body.backgroundColor === "") {
+	// 	body.classList.add("dark-mode");
+	// } else {
+	// 	body.classList.remove("dark-mode");
+	// }
+
+	// body.classList.add("dark-mode");
+
+	if (darkModeButton.style.backgroundColor === "black") {
+		darkModeButton.style.backgroundColor = "white";
+		darkModeButton.style.color = "black";
+		darkModeButton.style.border = "2px solid black";
+
+		headings.style.color = "black";
+		icon.style.filter = "invert(0)";
+		body.classList.remove("dark-mode");
+	} else {
+		darkModeButton.style.backgroundColor = "black";
+		darkModeButton.style.color = "white";
+		darkModeButton.style.border = "2px solid white";
+
+		headings.style.color = "white";
+		icon.style.filter = "invert(1)";
+		body.classList.add("dark-mode");
+		colorSelector.style.border = "1px solid white";
+	}
+});
