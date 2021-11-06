6e0477d5281c6d08043e3fdc8f8d7511c01f9d5a
Thomas4897 <t.maynard4897@gmail.com>
2021-11-06 15:52:44 +0800

    Adding Fill Function

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const gridWidth = 50;
+const gridWidth = 80;
+
+const canvas = document.querySelector(".canvas");
 
 let count = 0;
 while (count < gridWidth * gridWidth) {
-	const canvas = document.querySelector(".canvas");
 	const div = document.createElement("div");
 	div.className = "square color-5";
 	canvas.appendChild(div);
@@ -10,27 +11,51 @@ while (count < gridWidth * gridWidth) {
 }
 
 const app = document.querySelector(".app");
-
 const colorSelector = document.querySelector(".palette");
-
 const currentBrush = document.querySelector(".current-brush");
+const body = document.querySelector("body");
+const darkModeButton = document.querySelector(".dark-mode-button");
+const headings = document.querySelector(".headings");
+const icon = document.querySelector(".icon");
+const brushIcon = document.querySelector(".brush-icon");
+const fillIcon = document.querySelector(".fill-icon");
+const currentFill = document.querySelector(".current-fill");
+const square = document.querySelectorAll(".canvas .square");
 
 const newBrush = colorSelector.addEventListener("click", function (event) {
 	const currentColor = currentBrush.classList[1];
-	// console.log("event.target", event.target.classList[1]);
+
 	if (event.target.localName === "div") {
 		currentBrush.classList.replace(currentColor, event.target.classList[1]);
+		currentFill.classList.replace(
+			currentFill.classList[1],
+			event.target.classList[1]
+		);
+	}
+
+	if (currentBrush.classList[1] === "color-11") {
+		brushIcon.style.filter = "invert(1)";
+		fillIcon.style.filter = "invert(1)";
+	} else {
+		brushIcon.style.filter = "invert(0)";
+		fillIcon.style.filter = "invert(0)";
 	}
 });
-
-const canvas = document.querySelector(".canvas");
 
 const paint = canvas.addEventListener("click", function (event) {
 	const currentColor = currentBrush.classList[1];
 	const square = event.target.classList[1];
-	console.log("event.target", square);
+
 	if (event.target.localName === "div") {
 		event.target.classList.replace(square, currentColor);
+	}
+});
+
+const fill = currentFill.addEventListener("click", function () {
+	const currentColor = currentFill.classList[1];
+
+	for (let i = 0; i < gridWidth * gridWidth; i++) {
+		square[i].classList.replace(square[i].classList[1], currentColor);
 	}
 });
 
@@ -51,7 +76,7 @@ const mouseUp = document.addEventListener("mouseup", function () {
 const paintDrag = canvas.addEventListener("mousemove", function (event) {
 	const currentColor = currentBrush.classList[1];
 	const square = event.target.classList[1];
-	console.log("event.target", square);
+
 	if (clicked === true) {
 		if (event.target.localName === "div") {
 			event.target.classList.replace(square, currentColor);
@@ -59,18 +84,7 @@ const paintDrag = canvas.addEventListener("mousemove", function (event) {
 	}
 });
 
-const body = document.querySelector("body");
-
-const darkModeButton = document.querySelector(".dark-mode-button");
-
-const headings = document.querySelector(".headings");
-
-const icon = document.querySelector(".icon");
-
 darkModeButton.style.cursor = "pointer";
 const changeTheme = darkModeButton.addEventListener("click", function () {
-	const currentMode = body.classList[0];
-	console.log("Current Mode", currentMode);
-
 	body.classList.toggle("dark-mode");
 });
