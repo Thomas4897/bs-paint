4c5541edbd77cb2407787c295a1ca278da3661e0
Thomas4897 <t.maynard4897@gmail.com>
2021-11-06 12:15:57 +0800

    Simplifying Dark-Mode

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-/*******************
- * OUR HELPER CODE *
- *******************/
-
-/*
- * Here we add the squares to the canvas dynamically.
- * You can mostly leave this section alone!
- * But if you want to change how wide the canvas is,
- * there are just two steps:
- *
- * 1. Change the `gridWidth` value below.
- * 2. Change the `grid-template-rows` and
- * `grid-template-columns` to match.
- *
- * To make the second one happen, the number to change
- * is the first argument to `repeat`, currently set at 10.
- */
 const gridWidth = 50;
+
 let count = 0;
 while (count < gridWidth * gridWidth) {
 	const canvas = document.querySelector(".canvas");
@@ -50,16 +34,20 @@ const paint = canvas.addEventListener("click", function (event) {
 	}
 });
 
+//! Set clicked to false;
 let clicked = false;
 
+//! When mouseDown turns click true
 const mouseDown = document.addEventListener("mousedown", function () {
 	clicked = true;
 });
 
+//! When mouseUp turns click false
 const mouseUp = document.addEventListener("mouseup", function () {
 	clicked = false;
 });
 
+//! paintDrag continues if click is true;
 const paintDrag = canvas.addEventListener("mousemove", function (event) {
 	const currentColor = currentBrush.classList[1];
 	const square = event.target.classList[1];
@@ -81,30 +69,8 @@ const icon = document.querySelector(".icon");
 
 darkModeButton.style.cursor = "pointer";
 const changeTheme = darkModeButton.addEventListener("click", function () {
-	// if (body.backgroundColor === "") {
-	// 	body.classList.add("dark-mode");
-	// } else {
-	// 	body.classList.remove("dark-mode");
-	// }
+	const currentMode = body.classList[0];
+	console.log("Current Mode", currentMode);
 
-	// body.classList.add("dark-mode");
-
-	if (darkModeButton.style.backgroundColor === "black") {
-		darkModeButton.style.backgroundColor = "white";
-		darkModeButton.style.color = "black";
-		darkModeButton.style.border = "2px solid black";
-
-		headings.style.color = "black";
-		icon.style.filter = "invert(0)";
-		body.classList.remove("dark-mode");
-	} else {
-		darkModeButton.style.backgroundColor = "black";
-		darkModeButton.style.color = "white";
-		darkModeButton.style.border = "2px solid white";
-
-		headings.style.color = "white";
-		icon.style.filter = "invert(1)";
-		body.classList.add("dark-mode");
-		colorSelector.style.border = "1px solid white";
-	}
+	body.classList.toggle("dark-mode");
 });
